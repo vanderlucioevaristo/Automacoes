8708d1ceacab2fa4b4f96ee0ccc02ac22ae120e0
vander-evaristo_sinqia <vander.evaristo@ext.sinqia.com.br>
2025-09-10 14:53:16 -0300

    Update .gitignore to include .txt files and add VALIA entity to entidades.js

--- src/globals/entidades.js
+++ src/globals/entidades.js
@@ -8,6 +8,12 @@ const entidades = [
       "Basic NWZiNGNjOTEtNTViOS00YjE2LTk0ZmEtY2M4ZTM4YTg1MDU2OmQzNGRhMzYzLWRmZjktNDI3NC1hNTU3LWY1NGJlMzc3NjczZQ==", // Basic: valor a ser informado posteriormente
     clientKey: "5fb4cc91-55b9-4b16-94fa-cc8e38a85056", // ClientKey: valor a ser informado posteriormente
   },
+  {
+    nome: "VALIA", // Nome da entidade
+    basic:
+      "Basic ZGJiZjhjZjctMDQ5MC00NGQ3LTk0NjUtM2JhOWFkNWUzMGFjOjlmOTM0MDc4LThjNjMtNDBjZi1iNzkwLWM2YmQ2MWVmOTRiZg==", // Basic: valor a ser informado posteriormente
+    clientKey: "dbbf8cf7-0490-44d7-9465-3ba9ad5e30ac", // ClientKey: valor a ser informado posteriormente
+  }
   // Adicione novas entidades conforme necessário
 ];
 
